2c6a30defb0b70717ad033bd615783ef896ec62e
Lambert Le <lambert.le@ieee.org>
2012-10-01 19:02:58 -0400

    Test File
    Created a testfile to attempt merging. Alex and Lambert are trying it
    out.

---
+++ Kronos/Kronos/testGitConflict.aspx.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Data.Linq;
+using System.Configuration;
+using Kronos.CLASSES;
+using System.Net.Mail;
+using System.Net;
+namespace Kronos
+{
+    public partial class test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            register_label.Visible = false;
+        }
+
+        protected void testGitLam()
+        {
+            //Test! Lam
+        }
+
+        protected void testGitAlex()
+        {
+            //Test! Alex
+        }
+
+       
+
+        }
+}
